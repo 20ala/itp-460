d968ad168d601cd9ba08fa5c3cc7a934ff80f022
Gurdeep Barring <97579083+TheSikhAlchemist@users.noreply.github.com>
2023-11-28 22:38:42 -0800

    Add files via upload

---
+++ web_app/car_form.php
@@ -0,0 +1,422 @@
+<?php
+	$host = "304.itpwebdev.com";
+	$user = "traveleasy_user1";
+	$pass = "usc460itp";
+	$db = "traveleasy_info_db";
+
+	$mysqli = new mysqli($host, $user, $pass, $db);
+
+	if ($mysqli->connect_errno) {
+		echo $mysqli->connect_error;
+		exit();
+	}
+
+    $sql = "SELECT * FROM cities;";
+
+	$results = $mysqli->query($sql);
+
+	if (!$results) {
+		echo $mysqli->error;
+		$mysqli->close();
+		exit();
+	}
+
+	$sql_car = "SELECT * FROM car_models;";
+
+	$results_car = $mysqli->query($sql_car);
+
+	if (!$results_car) {
+		echo $mysqli->error;
+		$mysqli->close();
+		exit();
+	}
+
+
+	$mysqli->close();
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title> Search Page </title> 
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+        <style>   
+        body {
+            font-family: 'Open Sans', sans-serif;
+        }
+
+        #nav {
+            position: fixed; 
+            background-color: #EBEBEB; 
+            margin-left: auto; 
+            margin-right: auto;
+            height: 75px; 
+            top: 0px; 
+            left: 0px; 
+            right: 0px; 
+            z-index: 1; 
+        }
+
+        #home {
+            width: 300px; 
+            margin-left: 20px; 
+            padding: 10px;
+            text-align: left; 
+            font-size: 30px; 
+            text-decoration: none; 
+            position: relative; 
+            top: 20px; 
+            color: black; 
+        }
+
+        #home:hover {
+            color: white; 
+        }
+
+        #option {
+            width: 330px; 
+            margin: 0px; 
+        }
+    
+        #option li {
+            width: 90px; 
+            margin: 10px; 
+            line-height:22px;
+        }
+
+        #option a {
+            color: black; 
+            text-decoration: none; 
+            font-size: 20px; 
+        }
+
+        #option ul {
+            display: flex; 
+            list-style: none; 
+            position: absolute; 
+            right: 50px;
+            bottom: 0px; 
+        }
+
+        #option a:hover {
+            color: white; 
+        }
+
+       /* #flight_opt {
+            padding-right: 35px; 
+        } */
+
+        #hotel_opt{
+            padding-left: 20px;
+        }
+
+        #main {
+            margin-top: 90px; 
+            margin-left: auto;
+            margin-right: auto; 
+            width: 1200px; 
+            position: relative;
+            text-align: center;
+        }
+
+        #first {
+            width: 1100px; 
+            height: 600px; 
+            margin-left: auto;
+            margin-right: auto;
+            text-align: center;
+        }
+
+        #description {
+            font-size: 50px; 
+            color: white; 
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            text-shadow: 1px 1px 5px black;
+        }
+
+        #foot {
+            bottom: 0;
+            background-color: #EBEBEB;
+            text-align: center;
+            width: 100%;
+            height: 30px;
+            line-height: 30px;
+        }
+
+        #location {
+            margin-left: auto;
+            margin-right: auto;
+            display: block;
+            width: 800px;
+
+        }
+
+        #location_text {
+            width: 800px;
+            font-size: 1.5em;
+            letter-spacing: 3px;
+        }
+
+        #criteria {
+            text-align: center;
+            font-size: 2.5em;
+            margin-left: auto;
+            margin-right: auto;
+
+        }
+
+        .input_group {
+            margin-left: auto;
+            margin-right: auto;
+            width: 600px;
+            text-align: center;
+        }
+
+        #transportation {
+            width: 600px;
+        }
+
+        #continent {
+            width: 600px;
+        }
+
+        #price {
+            width: 600px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        #dates {
+            margin-left: auto;
+            margin-right: auto;
+            width: 600px;
+            
+        }
+
+        #models {
+            padding-top: 20px;
+            margin-left: auto;
+            margin-right: auto;
+            width: 600px;
+
+        }
+
+        .clearfloat {
+		    clear: both;
+	    }
+
+        #buttons {
+            padding-top: 25px;
+            margin-left: auto;
+            margin-right: auto;
+            text-align: center;
+        }
+
+        #buttons button {
+            margin-left: 15px;
+        }
+
+        #help {
+            width: 1115px;
+            margin-left: auto;
+            margin-right: auto;
+            background-color: whitesmoke;
+            padding-bottom: 50px;
+        }
+
+        #help h2, h3 {
+            margin-left: auto;
+            margin-right: auto;
+            text-align: center;
+        }
+
+        #imgs {
+            width: 1100px;
+            display: flex;
+        }
+
+        #one, #two, #three {
+            background-color: blue;
+            width: 345px;
+            margin: 10px;
+        }
+
+        .section {
+            width: 345px;
+            margin: 10px;
+            position: relative;
+            text-align: center;
+        }
+
+        .name {
+            font-size: 30px; 
+            color: white; 
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-40%, -50%);
+            text-shadow: 1px 1px 5px black;
+        }
+   
+
+        </style>
+    </head>
+
+    <body>
+        <!-- Navbar -->
+        <div id="nav">
+            <a id="home" href="homepage.php"> TRAVELEASY </a>
+    
+            <div id="option">            
+                <ul>
+                    <li id="car_opt"> <a href="car_form.php"> CARS </a> </li>
+                    <li id="flight_opt"> <a href="flight_form.php"> FLIGHTS </a> </li>
+                    <li id="hotel_opt"> <a href="hotel_form.php"> HOTELS </a> </li>
+                </ul>
+            </div>
+        </div>
+        <!-- End of Navbar -->
+
+        <!-- Header -->
+        <div id="main">
+            <img id="first" src="img/car_main.jpeg" alt="main_pic"> 
+
+            <p id ="description"> WHERE DO YOU WANT TO LOOK FOR CARS? </p>
+        </div>
+        <!-- End of Header  -->
+
+        <br>
+        <br>
+
+        <!-- Beginning of Content -->
+        <form action="car_search_results.php" method="GET">
+
+            
+            <div class="form-group row" id="location">
+				<div class="col-sm-9">
+					<select name="city_id" id="location_text" class="form-control">
+						<option value="" selected>Cities...</option>
+
+						<!-- TODO: Output all teams from the DB here. -->
+						<?php while ($row = $results->fetch_assoc()) : ?>
+							<option value="<?php echo $row['id']; ?>">
+								<?php echo $row['city_name']; ?>
+							</option>
+						<?php endwhile; ?>
+
+					</select>
+				</div>
+				<!-- <div class="col-sm-8">
+					<input id="location_text" type="text" class="form-control" placeholder="Enter City" name="city_id">
+				</div> -->
+			</div> <!-- .form-group -->
+
+        <br>
+        
+        <br>
+
+        <div class="form-group row" id="dates">
+            <label class="col-sm-3 col-form-label text-sm-right"> Starting Date: </label>
+            <div class="col-sm-8">
+                <div class="row">
+                    <div class="col">
+                        <label class="form-check-label my-1">
+                            <input type="date" class="form-control mt-2" name="from">
+                        </label>
+                    </div> <!-- .col -->
+                   <!--<div class="col">
+                        <label class="form-check-label my-1">
+                            To:
+                            <input type="date" class="form-control mt-2" name="to">
+                        </label>
+                    </div> <!-- .col -->
+                </div> <!-- .row -->
+            </div> <!-- .col -->
+        </div> <!-- .form-group -->
+
+        <br>
+
+        <div class="form-group row" id="price">
+            <label class="col-sm-3 col-form-label text-sm-right"> Starting Budget:</label>
+            <div class="col-sm-9">
+                <input type="input" class="form-control" name="price" placeholder="Search by starting price">
+            </div>
+        </div> <!-- .form-group -->
+
+        <div class="form-group row" id="models">
+            <label class="col-sm-3 col-form-label text-sm-right"> Car Model: </label>
+            <div class="col-sm-9">
+					<select name="model_id" id="car_model" class="form-control">
+						<option value="" selected>-- All --</option>
+
+						<!-- TODO: Output all venues from the DB here. -->
+						<?php while ($row = $results_car->fetch_assoc()) : ?>
+							<option value="<?php echo $row['id']; ?>">
+								<?php echo $row['model_name']; ?>
+							</option>
+						<?php endwhile; ?>
+					</select>
+				</div>
+        </div> <!-- .form-group -->
+
+        <div id="buttons">
+            <button type="submit" class="btn btn-primary">Search</button>
+            <button type="reset" class="btn btn-light">Reset</button>
+        </div>
+
+        </form>
+
+        <br>
+
+        <br>
+
+        <div id="help">
+            <br> 
+
+            <h2> Need help searching? </h2>
+
+            <br> 
+
+            <h3> The most popular student car rentals: </h3>
+
+            <br> 
+            
+            <div id="imgs">
+
+                <div class="section">
+                    <img id="one" src="img/civic2.jpeg" alt="civic">
+                    <p class="name"> Honda Civic </p>
+                </div>
+
+                <div class="section">
+                    <img id="two" src="img/jeep.jpeg" alt="jeep">
+                    <p class="name"> Jeep Wrangler </p>
+                </div>
+
+                <div class="section">
+                    <img id="three" src="img/subaru.jpeg" alt="subaru">
+                    <p class="name"> Subaru Crosstrek </p>
+                </div>
+            </div>
+        </div>
+
+        <br>
+
+
+       
+
+        <div id="foot"> 
+            <p> TRAVELEASY &copy; 2023 </p>
+        </div>
+
+    </body>
+</html>
---
+++ web_app/flight_search_results.php
@@ -0,0 +1,291 @@
+<?php
+	$host = "304.itpwebdev.com";
+	$user = "traveleasy_user1";
+	$pass = "usc460itp";
+	$db = "traveleasy_info_db";
+
+	$mysqli = new mysqli($host, $user, $pass, $db);
+
+	if ($mysqli->connect_errno) {
+		echo $mysqli->connect_error;
+		exit();
+	}
+
+	$sql = "SELECT flights.price AS 'price', 
+				flights.start_date AS 'start_date', 
+				airlines.airline_name AS 'airline',
+                team_alias_1.city_name AS 'from', 
+				team_alias_2.city_name AS 'to'  
+			FROM flights
+			LEFT JOIN airlines
+				ON flights.airline_id = airlines.id
+            LEFT JOIN cities AS team_alias_1
+				ON flights.from_city_id = team_alias_1.id
+			LEFT JOIN cities AS team_alias_2
+				ON flights.to_city_id = team_alias_2.id
+			WHERE 1 = 1";
+
+	if (isset($_GET['airline_id']) && trim($_GET['airline_id']) != '') {
+		$airline_id = $_GET['airline_id'];
+		$sql = $sql . " AND flights.airline_id = $airline_id";
+	}
+
+    if (isset($_GET['from_city_id']) && trim($_GET['from_city_id']) != '') {
+		$from_city_id = $_GET['from_city_id'];
+		$sql = $sql . " AND team_alias_1.id = $from_city_id";
+	}
+    if (isset($_GET['to_city_id']) && trim($_GET['to_city_id']) != '') {
+		$from_city_id = $_GET['to_city_id'];
+		$sql = $sql . " AND team_alias_2.id = $to_city_id";
+	}
+
+    if (isset($_GET['from']) && trim($_GET['from']) != '') {
+		$from = $_GET['from'];
+		$sql = $sql . " AND flights.start_date = $from";
+	}
+
+	if (isset($_GET['price']) && trim($_GET['price']) != '') {
+		$price = $_GET['price'];
+		$sql = $sql . " AND flights.price = $price";
+	}
+
+	$sql = $sql . ";";
+
+	$results = $mysqli->query($sql);
+
+	if (!$results) {
+		echo $mysqli->error;
+		$mysqli->close();
+		exit();
+	}
+
+	$mysqli->close();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> TravelFly Home </title> 
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+
+        <style>   
+        body {
+            font-family: 'Open Sans', sans-serif;
+        }
+
+        #nav {
+            position: fixed; 
+            background-color: #EBEBEB; 
+            margin-left: auto; 
+            margin-right: auto;
+            height: 75px; 
+            top: 0px; 
+            left: 0px; 
+            right: 0px; 
+            z-index: 1; 
+        }
+
+        #home {
+            width: 300px; 
+            margin-left: 20px; 
+            padding: 10px;
+            text-align: left; 
+            font-size: 30px; 
+            text-decoration: none; 
+            position: relative; 
+            top: 20px; 
+            color: black; 
+        }
+
+        #home:hover {
+            color: white; 
+        }
+
+        #option {
+            width: 300px; 
+            margin: 0px; 
+        }
+    
+        #option li {
+            width: 80px; 
+            margin: 10px; 
+        }
+
+        #flight_opt {
+            padding-left: 10px; 
+            padding-right: 25px; 
+        }
+
+        #option a {
+            color: black; 
+            text-decoration: none; 
+            font-size: 20px; 
+        }
+
+        #option ul {
+            display: flex; 
+            list-style: none; 
+            position: absolute; 
+            right: 50px;
+            bottom: 0px; 
+        }
+
+        #option a:hover {
+            color: white; 
+        }
+
+
+        #foot {
+            background-color:#EBEBEB; 
+            text-align: center; 
+            height: 30px;
+            line-height: 30px;
+        }
+
+        #table {
+            width: 1000px;
+            margin-top: 80px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        #table p {
+            margin-left: auto;
+            margin-right: auto;
+            text-align: center;
+        }
+
+        table {
+            margin-left: auto;
+            margin-right: auto;
+            margin-bottom: 30px; 
+        }
+
+      
+        .logo {
+            width: 90px;
+            height: 50px;
+        }
+        
+        .wifi {
+            width: 50px;
+            height: 50px;
+        }
+
+        table {
+        border-collapse: separate; 
+        border-spacing: 0; 
+        }
+
+        td, th {
+        margin-right: 10px;
+        }
+
+        table th {
+            margin-left: 10px; 
+        }
+
+        #pic {
+            margin-top: 100px; 
+            margin-left: auto;
+            margin-right: auto; 
+            width: 500px; 
+            position: relative;
+            text-align: center;
+        }
+
+        #res_pic {
+            width: 500px; 
+            margin-left: auto;
+            margin-right: auto;
+            text-align: center;
+        }
+
+        #pic h2{
+            font-size: 25px; 
+            color: white; 
+            position: absolute; 
+            top: 40%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+
+        </style>
+    </head>
+
+    <body>
+        <div id="nav">
+            <a id="home" href="homepage.php"> TRAVELEASY </a>
+    
+            <div id="option">            
+                <ul>
+                    <li id="car_opt"> <a href="car_form.php"> CARS </a> </li>
+                    <li id="flight_opt"> <a href="flight_form.php"> FLIGHTS </a> </li>
+                    <li id="hotel_opt"> <a href="hotel_form.php"> HOTELS </a> </li>
+                </ul>
+            </div>
+        </div>
+
+        <div id="pic">
+            <img id="res_pic" src="https://images.unsplash.com/photo-1433838552652-f9a46b332c40?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8fA%3D%3D" alt="results">
+            <h2> RESULTS </h2>
+        </div>
+        <div class="container">
+		<div class="row mb-4">
+			<div class="col-12 mt-4">
+				<a href="car_form.php" role="button" class="btn btn-primary">Back to Form</a>
+			</div> <!-- .col -->
+		</div> <!-- .row -->
+		<div class="row">
+			<div class="col-12">
+
+				<!-- TODO: Replace '1' with actual number of results -->
+				Showing <?php echo $results->num_rows; ?> result(s).
+
+			</div> <!-- .col -->
+			<div class="col-12">
+				<table class="table table-hover table-responsive mt-4">
+					<thead>
+						<tr>
+							<th>Date</th>
+							<th>Price</th>
+                            <th>Starting City</th>
+                            <th>Destination</th>
+							<th>Flights</th>
+						</tr>
+					</thead>
+					<tbody>
+
+						<!-- TODO: Loop through DB results and output them here. Modify or remove hard-coded output below. -->
+
+						<?php while ($row = $results->fetch_assoc()) : ?>
+						<tr>
+							<td><?php echo $row['start_date']; ?></td>
+							<td><?php echo "$",$row['price']; ?></td>
+							<td><?php echo $row['from']; ?></td>
+                            <td><?php echo $row['to']; ?></td>
+                            <td><?php echo $row['airline']; ?></td>
+                           
+						</tr>
+						<?php endwhile; ?>
+
+					</tbody>
+				</table>
+			</div> <!-- .col -->
+		</div> <!-- .row -->
+		<div class="row mt-4 mb-4">
+			<div class="col-12">
+				<a href="car_form.php" role="button" class="btn btn-primary">Back to Form</a>
+			</div> <!-- .col -->
+		</div> <!-- .row -->
+	</div> <!-- .container -->
+
+
+        <div id="foot"> 
+            <p> TRAVELEASY &copy; 2023 </p>
+        </div>
+
+    </body>
+</html>
